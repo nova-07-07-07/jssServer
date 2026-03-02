fe9ada6c7efec063964f42809fe8f5075aaf3870
nova-07-07-07 <snova234607ns@gmail.com>
2026-03-02 18:34:28 +0530

    Update success message in hello_world function

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'happy message , server is running successfully'
+    return 'happy message , server is running successfully the update is working '
 
 if __name__ == '__main__':
     app.run(debug=True)
