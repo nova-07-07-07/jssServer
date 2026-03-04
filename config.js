2474d58b817d7bef8df981214d3ecdd5ea1a51f2
nova-07-07-07 <nova234607ns@gmail.com>
2026-03-04 19:18:07 +0530

    Update API URL: https://merger-additionally-weeks-tell.trycloudflare.com

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-export const API_URL = "https://demographic-expert-sort-probability.trycloudflare.com";
+export const API_URL = "https://merger-additionally-weeks-tell.trycloudflare.com";
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'happy message , server is running successfully the update is working - yes'
+    return "<?php echo 'Hello from PHP'; ?>"
 
 if __name__ == '__main__':
     app.run(port=5000)
